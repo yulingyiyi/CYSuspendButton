fe96e1670828453ada5b2c6dceb931e32c62a6d6
SX <3504598733@qq.com>
2017-05-26 15:23:55 +0800

    pan 手势

--- CYSuspendButton/CYSuspendButton.m
+++ CYSuspendButton/CYSuspendButton.m
@@ -18,104 +18,199 @@
 @implementation CYSuspendButton
 - (id)initWithFrame:(CGRect)frame
 {
-	self = [super initWithFrame:frame];
-	if (self) {
-		_minY = KNavBar_h;
-		_maxY = KSHEIGHT - KTabBar_h;
-		// Initialization code
-	}
-	return self;
+    self = [super initWithFrame:frame];
+    if (self) {
+        _minY = KNavBar_h;
+        _maxY = KSHEIGHT - KTabBar_h;
+        // Initialization code
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+        [self addGestureRecognizer:pan];
+    }
+    return self;
 }
-
-//触摸-清扫
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-       _MoveEnabled = NO;
-	[super touchesBegan:touches withEvent:event];
-	if (!_MoveEnable) {
-		return;
-	}
-	UITouch *touch = [touches anyObject];
-	_beginpoint = [touch locationInView:self];
+- (void)pan:(UIGestureRecognizer *)gesture{
+    
+    switch (gesture.state) {
+        case UIGestureRecognizerStateBegan:
+        {
+            
+            _MoveEnabled = NO;
+            if (!_MoveEnable) {
+                return;
+            }
+            
+            _beginpoint = [gesture locationInView:self];
+        }
+            break;
+        case UIGestureRecognizerStateChanged:
+        {
+            
+            
+            _MoveEnabled = YES;//单击事件可用
+            
+            if (!_MoveEnable) {
+                return;
+            }
+            
+            CGPoint currentPosition = [gesture locationInView:self];
+            //偏移量
+            float offsetX = currentPosition.x - _beginpoint.x;
+            float offsetY = currentPosition.y - _beginpoint.y;
+            //移动后的中心坐标
+            self.center = CGPointMake(self.center.x + offsetX, self.center.y + offsetY);
+            
+            //x轴左右极限坐标
+            if (self.center.x > (self.superview.frame.size.width -KWidth /2)){
+                CGFloat x = self.superview.frame.size.width-self.frame.size.width/2;
+                self.center = CGPointMake(x, self.center.y + offsetY);
+            }else if (self.center.x < KWidth / 2){
+                CGFloat x = KWidth / 2;
+                self.center = CGPointMake(x, self.center.y + offsetY);
+            }
+            
+            //y轴上下极限坐标
+            //	if (self.center.y > (self.superview.frame.size.height-self.frame.size.height/2)) {
+            if (self.center.y > _maxY - KHeight / 2) {
+                CGFloat x = self.center.x;
+                CGFloat y = _maxY - KHeight / 2;
+                self.center = CGPointMake(x, y);
+            }else if (self.center.y <_minY + KHeight / 2){
+                CGFloat x = self.center.x;
+                CGFloat y = _minY + KHeight / 2;
+                self.center = CGPointMake(x, y);
+            }
+            
+        }
+            break;
+            
+            
+        case UIGestureRecognizerStateEnded:
+        {
+            
+            if (!_MoveEnable) {
+                return;
+            }
+            if (self.center.x >= self.superview.frame.size.width/2) {//向右侧移动
+                //偏移动画
+                [UIView beginAnimations:@"move" context:nil];
+                [UIView setAnimationDuration:.5];
+                [UIView setAnimationDelegate:self];
+                
+                self.frame=CGRectMake(self.superview.frame.size.width - KWidth,self.center.y- KHeight / 2,  KWidth , KHeight);
+                //提交UIView动画
+                [UIView commitAnimations];
+            }else{//向左侧移动
+                
+                [UIView beginAnimations:@"move" context:nil];
+                [UIView setAnimationDuration:.5];
+                [UIView setAnimationDelegate:self];
+                self.frame=CGRectMake(0.f,self.center.y- KHeight / 2, KWidth , KHeight);
+                //提交UIView动画
+                [UIView commitAnimations];
+            }
+            
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
 }
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-	
-	_MoveEnabled = YES;//单击事件可用
-	
-	if (!_MoveEnable) {
-		return;
-	}
-	UITouch *touch = [touches anyObject];
-	CGPoint currentPosition = [touch locationInView:self];
-	//偏移量
-	float offsetX = currentPosition.x - _beginpoint.x;
-	float offsetY = currentPosition.y - _beginpoint.y;
-	//移动后的中心坐标
-	self.center = CGPointMake(self.center.x + offsetX, self.center.y + offsetY);
-	
-	//x轴左右极限坐标
-	if (self.center.x > (self.superview.frame.size.width -KWidth /2)){
-		CGFloat x = self.superview.frame.size.width-self.frame.size.width/2;
-		self.center = CGPointMake(x, self.center.y + offsetY);
-	}else if (self.center.x < KWidth / 2){
-		CGFloat x = KWidth / 2;
-		self.center = CGPointMake(x, self.center.y + offsetY);
-	}
-	
-	//y轴上下极限坐标
-//	if (self.center.y > (self.superview.frame.size.height-self.frame.size.height/2)) {
-    if (self.center.y > _maxY - KHeight / 2) {
-		CGFloat x = self.center.x;
-		CGFloat y = _maxY - KHeight / 2;
-		self.center = CGPointMake(x, y);
-	}else if (self.center.y <_minY + KHeight / 2){
-		CGFloat x = self.center.x;
-		CGFloat y = _minY + KHeight / 2;
-		self.center = CGPointMake(x, y);
-	}
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-	if (!_MoveEnable) {
-		return;
-	}
-	if (self.center.x >= self.superview.frame.size.width/2) {//向右侧移动
-		//偏移动画
-		[UIView beginAnimations:@"move" context:nil];
-		[UIView setAnimationDuration:.5];
-		[UIView setAnimationDelegate:self];
-		
-		self.frame=CGRectMake(self.superview.frame.size.width - KWidth,self.center.y- KHeight / 2,  KWidth , KHeight);
-		//提交UIView动画
-		[UIView commitAnimations];
-	}else{//向左侧移动
-		
-		[UIView beginAnimations:@"move" context:nil];
-		[UIView setAnimationDuration:.5];
-		[UIView setAnimationDelegate:self];
-		self.frame=CGRectMake(0.f,self.center.y- KHeight / 2, KWidth , KHeight);
-		//提交UIView动画
-		[UIView commitAnimations];
-		
-	}
-	
-	//不加此句话，UIButton将一直处于按下状态
-	[super touchesEnded: touches withEvent: event];
-	
-}
+//
+////触摸-清扫
+//- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+//       _MoveEnabled = NO;
+//	[super touchesBegan:touches withEvent:event];
+//	if (!_MoveEnable) {
+//		return;
+//	}
+//	UITouch *touch = [touches anyObject];
+//	_beginpoint = [touch locationInView:self];
+//}
+//
+//- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+//
+//	_MoveEnabled = YES;//单击事件可用
+//
+//	if (!_MoveEnable) {
+//		return;
+//	}
+//	UITouch *touch = [touches anyObject];
+//	CGPoint currentPosition = [touch locationInView:self];
+//	//偏移量
+//	float offsetX = currentPosition.x - _beginpoint.x;
+//	float offsetY = currentPosition.y - _beginpoint.y;
+//	//移动后的中心坐标
+//	self.center = CGPointMake(self.center.x + offsetX, self.center.y + offsetY);
+//
+//	//x轴左右极限坐标
+//	if (self.center.x > (self.superview.frame.size.width -KWidth /2)){
+//		CGFloat x = self.superview.frame.size.width-self.frame.size.width/2;
+//		self.center = CGPointMake(x, self.center.y + offsetY);
+//	}else if (self.center.x < KWidth / 2){
+//		CGFloat x = KWidth / 2;
+//		self.center = CGPointMake(x, self.center.y + offsetY);
+//	}
+//
+//	//y轴上下极限坐标
+////	if (self.center.y > (self.superview.frame.size.height-self.frame.size.height/2)) {
+//    if (self.center.y > _maxY - KHeight / 2) {
+//		CGFloat x = self.center.x;
+//		CGFloat y = _maxY - KHeight / 2;
+//		self.center = CGPointMake(x, y);
+//	}else if (self.center.y <_minY + KHeight / 2){
+//		CGFloat x = self.center.x;
+//		CGFloat y = _minY + KHeight / 2;
+//		self.center = CGPointMake(x, y);
+//	}
+//}
+//
+//- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+//{
+//
+//	for (UIEvent *ev in touches) {
+//		NSLog(@"%@__", ev);
+//	}
+//
+//	if (!_MoveEnable) {
+//		return;
+//	}
+//	if (self.center.x >= self.superview.frame.size.width/2) {//向右侧移动
+//		//偏移动画
+//		[UIView beginAnimations:@"move" context:nil];
+//		[UIView setAnimationDuration:.5];
+//		[UIView setAnimationDelegate:self];
+//
+//		self.frame=CGRectMake(self.superview.frame.size.width - KWidth,self.center.y- KHeight / 2,  KWidth , KHeight);
+//		//提交UIView动画
+//		[UIView commitAnimations];
+//	}else{//向左侧移动
+//		
+//		[UIView beginAnimations:@"move" context:nil];
+//		[UIView setAnimationDuration:.5];
+//		[UIView setAnimationDelegate:self];
+//		self.frame=CGRectMake(0.f,self.center.y- KHeight / 2, KWidth , KHeight);
+//		//提交UIView动画
+//		[UIView commitAnimations];
+//		
+//	}
+//	
+//	//不加此句话，UIButton将一直处于按下状态
+//	[super touchesEnded: touches withEvent: event];
+//}
 
 //外界因素取消touch事件，如进入电话
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	
+    
 }
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 
 @end
